b8ef368f934f0ee263683ab042cdad9a5f1d382d
Emmanuel Ayomide Fatolu <125464454+Fatolu043@users.noreply.github.com>
2023-04-20 16:53:34 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,200 @@
+
+function copyBlur() {
+ copy_blur("filter: blur(1px);");
+ 
+  let a = document.getElementById("notification");
+  a.className = "show";
+  
+  setTimeout(function(){ 
+  a.className = a.className.replace("show", "");
+   }, 3000);
+}
+
+function copy_blur (css_code_1){
+  navigator.clipboard.writeText(css_code_1);
+}
+
+
+function copyBrightness() {
+ copy_brightness("filter: brightness(250%);");
+ 
+  let a = document.getElementById("notification");
+  a.className = "show";
+  
+  setTimeout(function(){ 
+  a.className = a.className.replace("show", "");
+   }, 3000);
+}
+
+function copy_brightness(css_code_2){
+  navigator.clipboard.writeText(css_code_2);
+}
+
+
+
+function copyContrast() {
+ copy_contrast("filter: contrast(180%);"); 
+ 
+ let a = document.getElementById("notification");
+  a.className = "show";
+  
+  setTimeout(function(){ 
+  a.className = a.className.replace("show", "");
+   }, 3000);
+}
+
+function copy_contrast (css_code_3){
+  navigator.clipboard.writeText(css_code_3);
+}
+
+
+
+function copyGrayScale() {
+ copy_grayscale("filter: grayscale(100%);");
+  
+  let a = document.getElementById("notification");
+  a.className = "show";
+  
+  setTimeout(function(){ 
+  a.className = a.className.replace("show", "");
+   }, 3000);
+}
+
+function copy_grayscale(css_code_4){
+  navigator.clipboard.writeText(css_code_4);
+}
+
+
+function copyHueRotate() {
+ copy_huerotate("filter: hue-rotate(180deg);");
+ 
+let a = document.getElementById("notification");
+  a.className = "show";
+  
+  setTimeout(function(){ 
+  a.className = a.className.replace("show", "");
+   }, 3000);
+}
+
+function copy_huerotate(css_code_5){
+  navigator.clipboard.writeText(css_code_5);
+}
+
+
+function copyInvert() {
+ copy_invert("filter: hue-rotate(180deg);");
+
+let a = document.getElementById("notification");
+  a.className = "show";
+  
+  setTimeout(function(){ 
+  a.className = a.className.replace("show", "");
+   }, 3000);
+}
+
+function copy_invert(css_code_6){
+  navigator.clipboard.writeText(css_code_6);
+}
+
+
+
+function copyOpacity(){
+ copy_opacity("filter: hue-rotate(180deg);");
+
+let a = document.getElementById("notification");
+  a.className = "show";
+  
+  setTimeout(function(){ 
+  a.className = a.className.replace("show", "");
+   }, 3000);
+}
+
+function copy_opacity(css_code_7){
+  navigator.clipboard.writeText(css_code_7);
+}
+
+
+
+function copySaturate(){
+ copy_saturate("filter: hue-rotate(180deg);");
+ 
+ let a = document.getElementById("notification");
+  a.className = "show";
+  
+  setTimeout(function(){ 
+  a.className = a.className.replace("show", "");
+   }, 3000);
+ 
+}
+
+function copy_saturate(css_code_8){
+  navigator.clipboard.writeText(css_code_8);
+}
+
+
+
+
+function copySepia(){
+ copy_sepia("filter: hue-rotate(180deg);");
+ 
+ let a = document.getElementById("notification");
+  a.className = "show";
+  
+  setTimeout(function(){ 
+  a.className = a.className.replace("show", "");
+   }, 3000);
+}
+
+function copy_sepia(css_code_9){
+  navigator.clipboard.writeText(css_code_9);
+}
+
+
+
+function copyShadow(){
+ copy_shadow("filter: hue-rotate(180deg);");
+ 
+let a = document.getElementById("notification");
+  a.className = "show";
+  
+  setTimeout(function(){ 
+  a.className = a.className.replace("show", "");
+   }, 3000);
+}
+
+function copy_shadow(css_code_10){
+  navigator.clipboard.writeText(css_code_10);
+}
+
+
+
+function copyDeepBlue(){
+ copy_deep_blue("filter: hue-rotate(180deg);");
+  
+  let a = document.getElementById("notification");
+  a.className = "show";
+  
+  setTimeout(function(){ 
+  a.className = a.className.replace("show", "");
+   }, 3000);
+}
+
+function copy_deep_blue(css_code_11){
+  navigator.clipboard.writeText(css_code_11);
+ } 
+  
+function copyLightBlue(){
+ copy_light_blue("filter: hue-rotate(180deg);");
+
+let a = document.getElementById("notification");
+  a.className = "show";
+  
+  setTimeout(function(){ 
+  a.className = a.className.replace("show", "");
+   }, 3000);
+}
+
+function copy_light_blue(css_code_12){
+  navigator.clipboard.writeText(css_code_12);
+}
+
